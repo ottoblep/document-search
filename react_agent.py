6187f79cf218bab587ccc18119ece9133ab8211c
Severin Lochschmidt <seviron53@gmail.com>
2023-09-12 23:43:23 +0200

    Add more agents

---
+++ react_agent.py
@@ -0,0 +1,32 @@
+from llm import *
+from langchain.utilities import DuckDuckGoSearchAPIWrapper
+from langchain.utilities import WikipediaAPIWrapper
+from langchain.tools import WikipediaQueryRun
+from langchain.agents import initialize_agent, AgentType, Tool
+
+search = DuckDuckGoSearchAPIWrapper()
+wiki = WikipediaAPIWrapper(top_k_result=2, doc_content_chars_max=512)
+
+tools = [
+    Tool(
+        name = "Search",
+        func=search.run,
+        description="useful for when you need to answer questions about fast changing or current events. You should ask targeted questions"
+    ),
+    Tool(
+        name = "Wiki",
+        func=wiki.run,
+        description="useful for looking up static and long standing facts and information about things."
+    ),
+]
+
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    max_iterations = 1, early_stopping_method = "generate", verbose=True, handle_parsing_errors=True)
+
+
+while True:
+    try:
+        agent.run("What are the ingredients of Haribo Happy Cola?")
+        break
+    except:
+        continue
