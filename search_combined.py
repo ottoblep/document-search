babe36d71393b80fb8a4045eb4d904a4913543f3
Severin Lochschmidt <seviron53@gmail.com>
2023-09-13 12:05:06 +0200

    Add web to document search

--- search_combined.py
+++ search_combined.py
@@ -6,6 +6,8 @@ from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain import LLMChain, PromptTemplate
+from langchain.utilities import DuckDuckGoSearchAPIWrapper
 
 faiss_database = "vectorstore"
 
@@ -13,19 +15,23 @@ print("Loading Vectorstore")
 vectorstore = FAISS.load_local(folder_path=faiss_database, embeddings=embeddings)
 
 qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectorstore.as_retriever())
-
-print("Loading Retriever")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
 wiki_retriever = WikipediaRetriever()
+search_wrapper = DuckDuckGoSearchAPIWrapper()
 
 while True:
   question = input("Ask a question: ")
-  print("Adding relevant wikipedia articles to search:")
-  wiki_docs = wiki_retriever.get_relevant_documents(query = question,)
-  text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-  wiki_docs = text_splitter.split_documents(wiki_docs)
-  vectorstore.add_documents(wiki_docs, embeddings = embeddings)
+  print("Retrieving wikipedia")
+  wiki_docs = wiki_retriever.get_relevant_documents(query = question)
+  new_docs = text_splitter.split_documents(wiki_docs)
+  vectorstore.add_documents(new_docs, embeddings = embeddings)
+  print("Retrieving search results")
+  search_texts = search_wrapper.run(query=question)
+  new_texts = text_splitter.split_text(search_texts)
+  print(new_texts)
+  vectorstore.add_texts(texts=new_texts, embeddings=embeddings)
   print("Extracting Context:")
-  docs = vectorstore.similarity_search(question, k = 3)
+  docs = vectorstore.similarity_search(question, k=3)
   print(docs)
   print("Generating answer")
   print(qa_chain({"query": question}))
