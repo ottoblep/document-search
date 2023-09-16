2d49f5e1c597b0492f198403222766355b646f4f
Severin Lochschmidt <seviron53@gmail.com>
2023-09-16 16:42:07 +0200

    Remove duplicated definitions

--- agent.py
+++ agent.py
@@ -3,9 +3,6 @@ from langchain.utilities import DuckDuckGoSearchAPIWrapper
 from langchain.utilities import WikipediaAPIWrapper
 from langchain.llms import LlamaCpp
 
-qna_model_file = "./models/llama-2-7b-chat.Q4_K_M.gguf"
-llm = LlamaCpp(model_path=qna_model_file, n_ctx=4096, n_threads=12, temperature=0.2)
-
 search = DuckDuckGoSearchAPIWrapper()
 wiki = WikipediaAPIWrapper(top_k_result=2, doc_content_chars_max=512)
 
